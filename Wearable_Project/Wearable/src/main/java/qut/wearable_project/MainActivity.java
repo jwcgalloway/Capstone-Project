0a0eef6567e0a8a3b46533a7388eeaf3440281e5
jwcgalloway <jwcgalloway@gmail.com>
2016-09-03 19:59:33 +1000

    Combined testing buttons into an "Add tile" button and "Remove tile" button
    Functions relating to installing the Band tile are now contained in the
    "InstallAsync" inner class.  The "ProjectClient" class should now only
    contain functions related to the actual functioning of the Band app.

--- Wearable_Project/Wearable/src/main/java/qut/wearable_project/MainActivity.java
+++ Wearable_Project/Wearable/src/main/java/qut/wearable_project/MainActivity.java
@@ -1,6 +1,8 @@
 package qut.wearable_project;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,10 +16,29 @@ import com.microsoft.band.BandException;
 import com.microsoft.band.BandInfo;
 import com.microsoft.band.BandPendingResult;
 import com.microsoft.band.ConnectionState;
+import com.microsoft.band.tiles.BandIcon;
+import com.microsoft.band.tiles.BandTile;
+import com.microsoft.band.tiles.pages.FlowPanelOrientation;
+import com.microsoft.band.tiles.pages.HorizontalAlignment;
+import com.microsoft.band.tiles.pages.Margins;
+import com.microsoft.band.tiles.pages.PageData;
+import com.microsoft.band.tiles.pages.PageLayout;
+import com.microsoft.band.tiles.pages.PageRect;
+import com.microsoft.band.tiles.pages.ScrollFlowPanel;
+import com.microsoft.band.tiles.pages.VerticalAlignment;
+import com.microsoft.band.tiles.pages.WrappedTextBlock;
+import com.microsoft.band.tiles.pages.WrappedTextBlockData;
+import com.microsoft.band.tiles.pages.WrappedTextBlockFont;
+
+import java.util.UUID;
 
 /**
  * @author James Galloway
+ *
+ * TODO Project name
  * TODO This description
+ * TODO User messages
+ * TODO Persistence between values when app is run multiple times
  */
 public class MainActivity extends AppCompatActivity {
     private ProjectClient projectClient;
@@ -31,91 +52,92 @@ public class MainActivity extends AppCompatActivity {
 
         statusTst = Toast.makeText(MainActivity.this, null, Toast.LENGTH_LONG);
 
-        /* Connect Button */
-        Button connectBtn = (Button) findViewById(R.id.connectBtn);
-        connectBtn.setOnClickListener(new View.OnClickListener() {
+        /* Install Button */
+        Button installBtn = (Button) findViewById(R.id.installBtn);
+        installBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConnectAsync connection = new ConnectAsync();
-                connection.execute();
+                InstallAsync installation = new InstallAsync();
+                installation.execute();
             }
         });
 
-        /* Create Tile Button */
-        Button createBtn = (Button) findViewById(R.id.createBtn);
-        createBtn.setOnClickListener(new View.OnClickListener() {
+        /* Uninstall Button */
+        Button uninstallBtn = (Button) findViewById(R.id.uninstallBtn);
+        uninstallBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        projectClient.createTile();
-                    }
-                });
-            }
-        });
-
-        /* Remove Tile Button */
-        Button removeBtn = (Button) findViewById(R.id.removeBtn);
-        removeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        projectClient.removeTile();
-                    }
-                });
-            }
-        });
-
-        /* Send Dialog Button */
-        Button sendDiagBtn = (Button) findViewById(R.id.sendDiagBtn);
-        sendDiagBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        projectClient.sendDialog("Title", "Content - Ye boi");
-                    }
-                });
-            }
-        });
-
-        /* Setup Pages Button */
-        Button setupBtn = (Button) findViewById(R.id.setupBtn);
-        setupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        projectClient.setPageContent();
-                    }
-                });
+                String response;
+                if (projectClient.removeTile()) {
+                    response = "Wearable Project tile has been successfully removed";
+                } else {
+                    response = "Could not remove Wearable Project tile";
+                    projectClient.sendDialog("Uninstall", response);
+                }
+                statusTst.setText(response);
+                statusTst.show();
             }
         });
     }
 
+    enum TileLayoutIndex {
+        MessagesLayout
+    }
+
+    enum TileMessagesPageElementId {
+        Message
+    }
+
     /**
      * @author James Galloway
-     * Private class which connects to the Microsoft Band the phone is paired with.
+     * Private class with functions required to install the Band application.
+     *
+     * TODO +1s after ordinal calls - enum
      */
-    private class ConnectAsync extends AsyncTask<Void, Void, Boolean> {
+    private class InstallAsync extends AsyncTask<Void, Void, Boolean> {
         private String response;
+        BandClient bandClient;
+        UUID tileId;
+        UUID page1Id;
 
         /**
-         * Attempt to connect to band
-         * @param params Void
-         * @return True if successful, otherwise false
+         * Installs the Band application by connecting to the device, creating a tile and
+         * configuring the tile's pages.
+         *
+         * @param params Void.
+         * @return True if the installation was successful, otherwise false.
          */
         @Override
         protected Boolean doInBackground(Void...params) {
+            return connectToBand() && createTile() && setPageContent();
+        } // end doInBackground
+
+        /**
+         * Initialise a ProjectClient if the installation was successful.  Set status toast
+         * test and show toast.
+         *
+         * @param installed True if the application was successfully installed, otherwise false.
+         */
+        @Override
+        protected void onPostExecute(Boolean installed) {
+            if (installed) {
+                projectClient = new ProjectClient(bandClient, tileId, page1Id);
+                projectClient.sendDialog("Installation Successful", "And then some other message");
+                response = "Installation Successful";
+            }
+            statusTst.setText(response);
+            statusTst.show();
+        } // end onPostExecute
+
+        /**
+         * Attempt to connect to Band.
+         *
+         * @return True if successful, otherwise false.
+         */
+        private boolean connectToBand() {
             BandInfo[] pairedBands = BandClientManager.getInstance().getPairedBands();
 
-            // Catch error if no paired bands were found
-            BandClient bandClient;
+            // Catch error if no paired Band(s) were found
             try {
                 bandClient = BandClientManager.getInstance().create(MainActivity.this, pairedBands[0]);
             } catch (IndexOutOfBoundsException ex) {
@@ -124,13 +146,11 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
 
+            // Attempt to connect to Band
             BandPendingResult<ConnectionState> pendingResult = bandClient.connect();
-
             try {
                 ConnectionState state = pendingResult.await();
                 if (state == ConnectionState.CONNECTED) {
-                    projectClient = new ProjectClient(bandClient, MainActivity.this);
-                    response = "Connected";
                     return true;
                 } else {
                     response = "Connection Failed";
@@ -141,16 +161,87 @@ public class MainActivity extends AppCompatActivity {
                 response = ex.getMessage();
                 return false;
             }
-        }
+        } // end connectToBand
 
         /**
-         * Set toast text and show toast
-         * @param connected True if connected, otherwise false
+         * Creates the project tile on the Band.
+         *
+         * @return True if the tile was successfully created, otherwise false.
          */
-        @Override
-        protected void onPostExecute(Boolean connected) {
-            statusTst.setText(response);
-            statusTst.show();
-        }
-    } // end ConnectAsync
+        private boolean createTile() {
+            Bitmap smallIconBitmap = Bitmap.createBitmap(24, 24, Bitmap.Config.ARGB_8888);
+            BandIcon smallIcon = BandIcon.toBandIcon(smallIconBitmap);
+
+            Bitmap largeIconBitmap = Bitmap.createBitmap(48, 48, Bitmap.Config.ARGB_8888);
+            BandIcon largeIcon = BandIcon.toBandIcon(largeIconBitmap);
+
+            tileId = UUID.randomUUID();
+            PageLayout layout = createLayout();
+
+            BandTile tile = new BandTile.Builder(tileId, "Wearable", largeIcon)
+                    .setTileSmallIcon(smallIcon)
+                    .setPageLayouts(layout)
+                    .build();
+
+            try {
+                bandClient.getTileManager().addTile(MainActivity.this, tile).await();
+                return true;
+            } catch (InterruptedException | BandException ex) {
+                ex.printStackTrace();
+                response = ex.getMessage();
+                return false;
+            }
+        } // end createTile
+
+        /**
+         * Creates the layout of the pages on the Band.
+         *
+         * @return Layout of the pages.
+         */
+        private PageLayout createLayout() {
+            /* Scrollable vertical panel */
+            ScrollFlowPanel panel = new ScrollFlowPanel(new PageRect(0, 0, 245, 102));
+            panel.setFlowPanelOrientation(FlowPanelOrientation.VERTICAL);
+            panel.setHorizontalAlignment(HorizontalAlignment.LEFT);
+            panel.setVerticalAlignment(VerticalAlignment.TOP);
+
+            /* Text block */
+            WrappedTextBlock textBlock1 =
+                    new WrappedTextBlock(new PageRect(0, 0, 245, 102), WrappedTextBlockFont.SMALL);
+            textBlock1.setId(TileMessagesPageElementId.Message.ordinal() + 1);
+            textBlock1.setMargins(new Margins(15, 0, 15, 0));
+            textBlock1.setColor(Color.WHITE);
+            textBlock1.setAutoHeightEnabled(true);
+            textBlock1.setHorizontalAlignment(HorizontalAlignment.LEFT);
+            textBlock1.setVerticalAlignment(VerticalAlignment.TOP);
+
+            panel.addElements(textBlock1);
+            return new PageLayout(panel);
+        } // end createLayout
+
+        /**
+         * Sets the initial content of the pages on the Band.
+         *
+         * @return True if the page content was set successfully, otherwise false.
+         */
+        private boolean setPageContent() {
+            page1Id = UUID.randomUUID();
+
+            WrappedTextBlockData TBData =
+                    new WrappedTextBlockData(TileMessagesPageElementId.Message.ordinal() + 1, "Ye Boi");
+
+            PageData data =
+                    new PageData(page1Id, TileLayoutIndex.MessagesLayout.ordinal()).update(TBData);
+
+            try {
+                bandClient.getTileManager().setPages(tileId, data).await();
+                return true;
+            } catch (InterruptedException | BandException ex) {
+                ex.printStackTrace();
+                response = ex.getMessage();
+                return false;
+            }
+        } // end setPageContent
+
+    } // end InstallAsync
 }
--- Wearable_Project/Wearable/src/main/java/qut/wearable_project/ProjectClient.java
+++ Wearable_Project/Wearable/src/main/java/qut/wearable_project/ProjectClient.java
@@ -1,139 +1,48 @@
 package qut.wearable_project;
 
-import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-
 import com.microsoft.band.BandClient;
 import com.microsoft.band.BandException;
-import com.microsoft.band.tiles.BandIcon;
-import com.microsoft.band.tiles.BandTile;
-import com.microsoft.band.tiles.pages.FlowPanelOrientation;
-import com.microsoft.band.tiles.pages.HorizontalAlignment;
-import com.microsoft.band.tiles.pages.Margins;
-import com.microsoft.band.tiles.pages.PageData;
-import com.microsoft.band.tiles.pages.PageLayout;
-import com.microsoft.band.tiles.pages.PageRect;
-import com.microsoft.band.tiles.pages.ScrollFlowPanel;
-import com.microsoft.band.tiles.pages.VerticalAlignment;
-import com.microsoft.band.tiles.pages.WrappedTextBlock;
-import com.microsoft.band.tiles.pages.WrappedTextBlockData;
-import com.microsoft.band.tiles.pages.WrappedTextBlockFont;
 
 import java.util.UUID;
 
 /**
  * @author James Galloway
  * TODO This description
- * TODO Exceptions & error checking
- * TODO +1s after ordinal calls - enum
  */
 class ProjectClient {
     private final BandClient projectClient;
-    private final UUID tileID;
-    private final UUID page1;
-    private final Activity activity;
+    private final UUID tileId;
+    private final UUID page1Id;
 
-    enum TileLayoutIndex {
-        MessagesLayout
-    }
-
-    enum TileMessagesPageElementId {
-        Message
-    }
-
-    ProjectClient(BandClient c, Activity a) {
+    ProjectClient(BandClient c, UUID tId, UUID p1Id) {
         projectClient = c;
-        activity = a;
-
-        tileID = UUID.randomUUID();
-        page1 = UUID.randomUUID();
+        tileId = tId;
+        page1Id = p1Id;
     }
 
     /**
-     * Creates the project tile on the band
+     * Removes the project tile from the Band.
+     * @return True if the tile was successfully remove, otherwise false
      */
-    void createTile() {
-        Bitmap smallIconBitmap = Bitmap.createBitmap(24, 24, Bitmap.Config.ARGB_8888);
-        BandIcon smallIcon = BandIcon.toBandIcon(smallIconBitmap);
-
-        Bitmap largeIconBitmap = Bitmap.createBitmap(48, 48, Bitmap.Config.ARGB_8888);
-        BandIcon largeIcon = BandIcon.toBandIcon(largeIconBitmap);
-
-        PageLayout layout = createLayout();
-
-        BandTile tile = new BandTile.Builder(tileID, "Wearable", largeIcon)
-                .setTileSmallIcon(smallIcon)
-                .setPageLayouts(layout)
-                .build();
-
+    boolean removeTile() {
         try {
-            projectClient.getTileManager().addTile(activity, tile).await();
-        } catch (InterruptedException | BandException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    /**
-     * Removes the project tile from the band
-     */
-    void removeTile() {
-        try {
-            projectClient.getTileManager().removeTile(tileID).await();
+            projectClient.getTileManager().removeTile(tileId).await();
+            return true;
         } catch (BandException | InterruptedException ex) {
             ex.printStackTrace();
+            return false;
         }
     }
 
     /**
      * Sends a dialog to the Band.
+     *
      * @param title The title of the dialog
      * @param msg The content of the dialog
      */
     void sendDialog(String title, String msg) {
         try {
-            projectClient.getNotificationManager().showDialog(tileID, title, msg).await();
-        } catch (InterruptedException | BandException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    /**
-     * Creates the layout of the pages on the Band.
-     * @return Layout of the pages
-     */
-    private PageLayout createLayout() {
-        /* Scrollable vertical panel */
-        ScrollFlowPanel panel = new ScrollFlowPanel(new PageRect(0, 0, 245, 102));
-        panel.setFlowPanelOrientation(FlowPanelOrientation.VERTICAL);
-        panel.setHorizontalAlignment(HorizontalAlignment.LEFT);
-        panel.setVerticalAlignment(VerticalAlignment.TOP);
-
-        /* Text block */
-        WrappedTextBlock textBlock1 =
-                new WrappedTextBlock(new PageRect(0, 0, 245, 102), WrappedTextBlockFont.MEDIUM);
-        textBlock1.setId(TileMessagesPageElementId.Message.ordinal() + 1);
-        textBlock1.setMargins(new Margins(15, 0, 15, 0));
-        textBlock1.setColor(Color.WHITE);
-        textBlock1.setAutoHeightEnabled(true);
-        textBlock1.setHorizontalAlignment(HorizontalAlignment.LEFT);
-        textBlock1.setVerticalAlignment(VerticalAlignment.TOP);
-
-        panel.addElements(textBlock1);
-        return new PageLayout(panel);
-    }
-
-    /**
-     * Sets the initial content of the pages on the Band
-     */
-    void setPageContent() {
-        WrappedTextBlockData TBData =
-                new WrappedTextBlockData(TileMessagesPageElementId.Message.ordinal() + 1, "Message One - Ye boi");
-
-        PageData data =
-                new PageData(page1, TileLayoutIndex.MessagesLayout.ordinal()).update(TBData);
-        try {
-           projectClient.getTileManager().setPages(tileID, data).await();
+            projectClient.getNotificationManager().showDialog(tileId, title, msg).await();
         } catch (InterruptedException | BandException ex) {
             ex.printStackTrace();
         }
