ee5c849f56a1560641d0fd4cab7ccc1436a8941e
jwcgalloway <jwcgalloway@gmail.com>
2016-09-17 15:02:34 +1000

    Movement counter working
    - Implemented working movement counter (may be too sensitive but can
    massage values to get the desired result)

--- Wearable_Remake/app/src/main/java/qut/wearable_remake/ProjectAccelerometer.java
+++ Wearable_Remake/app/src/main/java/qut/wearable_remake/ProjectAccelerometer.java
@@ -1,7 +1,5 @@
 package qut.wearable_remake;
 
-import android.util.Log;
-
 import com.microsoft.band.BandClient;
 import com.microsoft.band.BandIOException;
 import com.microsoft.band.sensors.BandAccelerometerEvent;
@@ -11,8 +9,10 @@ import com.microsoft.band.sensors.SampleRate;
 class ProjectAccelerometer implements ProjectSensor {
     private final ProjectClient projectClient;
     private final BandAccelerometerEventListener listener;
+    private static final long BOUNCE_TIME = 750;
+    private long lastActivated;
     private boolean moving;
-    private int offset;
+    private float offset;
 
     ProjectAccelerometer(final ProjectClient pc, final SpecialEventListener specialEvent) {
         projectClient = pc;
@@ -25,9 +25,6 @@ class ProjectAccelerometer implements ProjectSensor {
                 float x = bandEvent.getAccelerationX();
                 float y = bandEvent.getAccelerationY();
                 float z = bandEvent.getAccelerationZ();
-                Log.d("X:", Float.toString(x));
-                Log.d("Y:", Float.toString(y));
-                Log.d("Z:", Float.toString(z));
                 long time = bandEvent.getTimestamp();
 
                 if (projectClient.getProjectContact().getWorn()) {
@@ -35,24 +32,16 @@ class ProjectAccelerometer implements ProjectSensor {
                     specialEvent.onAccChanged(accData, time);
                 }
 
-                // TODO 
                 float sum = x + y + z;
-                Log.d("Offset: ", Integer.toString(offset));
-                Log.d("Sum: ", Float.toString(sum));
-                if (sum + offset > 1.5 || sum + offset < -1.5 && !moving) {
+                if ((sum - offset > 0.3 || sum - offset < -0.3) && !moving
+                        && bandEvent.getTimestamp() > lastActivated + BOUNCE_TIME) {
+                    lastActivated = bandEvent.getTimestamp();
+
                     moving = true;
                     projectClient.setMoveCount(projectClient.getMoveCount() + 1);
                 } else {
                     moving = false;
-                    if (z > 0.8) { // Flat Up
-                        offset = 1;
-                    } else if (y < -0.8) { // Left Tilt
-                        offset = -1;
-                    } else if (y > 0.8) { // Right Tilt
-                        offset = 1;
-                    } else if (z < -0.8) { // Flat Down
-                        offset = -1;
-                    }
+                    offset = sum;
                 }
             }
         };
