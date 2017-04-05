f6ce56311c79d1d99556d42df53e827629a50b4d
Arjai Reynolds <arjaireynolds@Arjais-MacBook-Pro.local>
2017-04-05 19:26:58 +1000

    Filtering time and rounding x,y,z to 4 decimal places

---
+++ Python_Filter_Scripts/filterTimeScript.py
@@ -0,0 +1,24 @@
+import csv
+
+def formatString(row):
+    list  = []
+    list.append(row[0])
+    list.append(str(round(float(row[1]),4)))
+    list.append(str(round(float(row[2]),4)))
+    list.append(str(round(float(row[3]),4)))
+    return list
+with open('rawdata.csv','rb') as csvfile,open('outputFile.csv','wb') as output :
+    reader = csv.reader(csvfile)
+    writer = csv.writer(output)
+    stored = 0
+    accum = 0
+    for row in reader:  #print row[0]
+        epoch = int(row[0])
+        difference = epoch - stored
+        accum += difference
+        if accum > 95:
+            accum = 0
+            writer.writerow((formatString(row)))
+        stored = int(row[0])
+
+
